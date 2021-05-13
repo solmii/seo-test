8025a962633f013afac2f66bb549b3c6070f1286
tina <tina@contxts.io>
2021-05-13 17:27:52 +0900

    wip2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>NFTBank.ai</title>
+        <meta name='title' content='NFTBank.ai'></meta>
+        <meta name='description' content='Portfolio - Search and manage Ethereum NFT portfolio on NFTBank.'></meta>
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property='og:type' content='website'></meta>
+        <meta property='og:url' content='https://solmii.github.io/seo-test/'></meta>
+        <meta property='og:title' content='NFTBank.ai'></meta>
+        <meta
+          property='og:description'
+          content='Portfolio - Search and manage Ethereum NFT portfolio on NFTBank.'
+        ></meta>
+        <meta
+          property='og:image'
+          content='https://storage.googleapis.com/nftbank-public/nftbank-public/default.png'
+        ></meta>
+
+        {/* <!-- Twitter --> */}
+        <meta property='twitter:card' content='summary_large_image'></meta>
+        <meta property='twitter:url' content='https://solmii.github.io/seo-test/'></meta>
+        <meta property='twitter:title' content='NFTBank.ai'></meta>
+        <meta
+          property='twitter:description'
+          content='Portfolio - Search and manage Ethereum NFT portfolio on NFTBank.'
+        ></meta>
+        <meta
+          property='twitter:image'
+          content='https://storage.googleapis.com/nftbank-public/nftbank-public/default.png'
+        ></meta>
+      </Head>
+
+      {/*         
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>NFTBank.ai | Portfolio</title>
         <meta name="title" content="NFTBank.ai | Portfolio" />
@@ -26,60 +59,52 @@ export default function Home() {
         <meta property="twitter:title" content="NFTBank.ai | Portfolio" />
         <meta property="twitter:description" content="Portfolio - Search and manage Ethereum NFT portfolio on NFTBank." />
         <meta property="twitter:image" content="https://storage.googleapis.com/nftbank-public/nftbank-public/default.png" />
-        <meta name="twitter:site" content={"@NFTbank_ai"} />
-      </Head>
+        <meta name="twitter:site" content={"@NFTbank_ai"} /> */}
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href='https://nextjs.org/docs' className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href='https://nextjs.org/learn' className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href='https://github.com/vercel/next.js/tree/master/examples' className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
